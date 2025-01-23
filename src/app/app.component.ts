79cc0749efd24414429573231862f87576f4f423
mxeka <mariza.xeka@ikubinfo.al>
2025-01-23 01:11:38 +0100

    update layout structure moving layout to main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
+import { UserRole } from './core/enums/user-role.enum';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [  
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'expense-management';
+    private authService = inject(AuthService);
+    currentUser$ = this.authService.currentUser$;
+    UserRole = UserRole;
+  
+    logout(): void {
+      this.authService.logout();
+    }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,8 @@
-// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { UserRole } from './core/enums/user-role.enum';
 import { RoleGuard } from './core/guards/role.guard';
-import { AuthGuard } from './core/guards/auth.guard';
-import { LayoutRouterComponent } from './layout/layout-router';
-import { LayoutComponent } from './layout/layout/layout.component';
 
 
 export const routes: Routes = [
@@ -17,69 +12,60 @@ export const routes: Routes = [
         import('./auth/login/login.component').then(m => m.LoginComponent)
     },
     {
-      path: 'main',
-      component: LayoutComponent,
-      canActivate: [AuthGuard],
-      canActivateChild: [AuthGuard],
+      path: 'trips',
+      canActivate: [RoleGuard],
+      data: { roles: [UserRole.END_USER] },
       children: [
         {
-          path: 'trips',
-          component: LayoutRouterComponent,
-          children: [
-            {
-              path: '',
-              loadComponent: () =>
-                import('./features/trips/trip-list/trip-list.component').then(m => m.TripListComponent)
-            },
-            {
-              path: 'new',
-              loadComponent: () =>
-                import('./features/trips/trip-form/trip-form.component').then(m => m.TripFormComponent)
-            },
-            {  // Add this route for trip details
-                path: ':id',
-                loadComponent: () =>
-                  import('./features/trips/trip-detail-view/trip-detail-view.component').then(m => m.TripDetailViewComponent)
-              }
-          ]
+          path: '',
+          loadComponent: () => import('./features/trips/trip-list/trip-list.component')
+            .then(m => m.TripListComponent)
+        },
+        { 
+          path: 'new',
+          loadComponent: () => import('./features/trips/trip-form/trip-form.component')
+            .then(m => m.TripFormComponent)
         },
         {
-          path: 'approvals',
-          component: LayoutRouterComponent,
-          canActivate: [RoleGuard],
-          data: { roles: [UserRole.APPROVER] },
-          children: [
-            {
-              path: '',
-              loadComponent: () =>
-                import('./features/approval/approvals-list/approvals-list.component').then(m => m.ApprovalsListComponent)
-            },
-            {
-              path: ':id',
-              loadComponent: () =>
-                import('./features/trips/trip-detail-view/trip-detail-view.component').then(m => m.TripDetailViewComponent)
-            }
-          ]
-        },
-        {
-          path: 'finance',
-          component: LayoutRouterComponent,
-          canActivate: [RoleGuard],
-          data: { roles: [UserRole.FINANCE] },
-          children: [
-            {
-              path: '',
-              loadComponent: () =>
-                import('./features/finance/finance-dashboard/finance-dashboard.component').then(m => m.FinanceDashboardComponent)
-            }
-          ]
+          path: ':id',
+          loadComponent: () => import('./features/trips/trip-detail-view/trip-detail-view.component')
+            .then(m => m.TripDetailViewComponent)
         }
+      ]
+    },
+    {
+      path: 'approvals',
+      canActivate: [RoleGuard],
+      data: { roles: [UserRole.APPROVER] },
+      children: [
+        {
+          path: '',
+          loadComponent: () =>
+            import('./features/approval/approvals-list/approvals-list.component').then(m => m.ApprovalsListComponent),
+        },
+        {
+          path: ':id',
+          loadComponent: () =>
+            import('./features/approval/approval-detail-view/approval-detail-view.component').then(m => m.ApprovalDetailViewComponent)
+        }
+      ]
+    },
+    {
+      path: 'finance',
+      canActivate: [RoleGuard],
+      data: { roles: [UserRole.FINANCE] },
+      children: [
+        {
+          path: '',
+          loadComponent: () =>
+            import('./features/finance/finance-view/finance-view.component').then(m => m.FinanceViewComponent),
+        },
       ]
     }
   ];
   
   @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, { enableTracing: true })],
     exports: [RouterModule]
   })
   export class AppRoutingModule { }
