df8a23cd98c56ae99f83759c342c1873cb5d0472
mxeka <mariza.xeka@ikubinfo.al>
2025-01-24 04:09:54 +0100

    added change to layout and app route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,18 @@
-import { Component, inject } from '@angular/core';
+import { Component} from '@angular/core';
+import { LayoutComponent } from './layout/layout/layout.component';
 import { RouterOutlet } from '@angular/router';
-import { AuthService } from './core/services/auth.service';
-import { UserRole } from './core/enums/user-role.enum';
-import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [  
-    CommonModule,
-    RouterOutlet,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule],
+    LayoutComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'expense-management';
-    private authService = inject(AuthService);
-    currentUser$ = this.authService.currentUser$;
-    UserRole = UserRole;
-  
-    logout(): void {
-      this.authService.logout();
-    }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,11 @@ import { RoleGuard } from './core/guards/role.guard';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
     loadComponent: () =>
       import('./auth/login/login.component').then(m => m.LoginComponent),
   },
